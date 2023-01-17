8ebdc8a383f9b00ccd9acc0c342d3f75fd815217
SantiagoBruno <brunosantiga@gmail.com>
2023-01-17 11:09:07 -0300

    refactor: indentation correction

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -3,28 +3,28 @@ import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
 
 describe('SignUp Routes', () => {
-    beforeAll(async () => {
-        await MongoHelper.connect(process.env.MONGO_URL)
-    })
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
 
-    afterAll(async () => {
-        await MongoHelper.disconnect()
-    })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
 
-    beforeEach(async () => {
-        const accountCollection = await MongoHelper.getCollection('account')
-        await accountCollection.deleteMany({})
-    })
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
+  })
 
-    test('Should return an account on success', async () => {
-        await request(app)
-        .post('/api/signup')
-        .send({
-            name: 'valid_name', 
-            email: 'email_valido@email.com', 
-            password: 'valid_password',
-            passwordConfirmation: 'valid_password'
-        })
-        .expect(200)
-    })
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      })
+      .expect(200)
+  })
 })
