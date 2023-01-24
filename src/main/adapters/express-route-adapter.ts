1734d744580bf0ba5677c2c1d29e0db8d71588ac
SantiagoBruno <brunosantiga@gmail.com>
2023-01-23 21:11:24 -0300

    fix: adptRoute bug with void async

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -2,15 +2,17 @@ import { Controller, HttpRequest, HttpResponse } from '../../presentation/protoc
 import { Request, Response } from 'express'
 
 export const adptRoute = (controller: Controller) => {
-  return async (req: Request, res: Response) => {
-    const httpRequest: HttpRequest = {
-      body: req.body
-    }
-    const httpResponse: HttpResponse = await controller.handle(httpRequest)
-    if (httpResponse.statusCode === 200) {
-      res.status(httpResponse.statusCode).json(httpResponse.body)
-    } else {
-      res.status(httpResponse.statusCode).json(httpResponse.body.message)
-    }
+  return (req: Request, res: Response) => {
+    void (async () => {
+      const httpRequest: HttpRequest = {
+        body: req.body
+      }
+      const httpResponse: HttpResponse = await controller.handle(httpRequest)
+      if (httpResponse.statusCode === 200) {
+        res.status(httpResponse.statusCode).json(httpResponse.body)
+      } else {
+        res.status(httpResponse.statusCode).json(httpResponse.body.message)
+      }
+    })
   }
 }
