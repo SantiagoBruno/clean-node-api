8c979161ed0779c34ab40199b20df3be33bae83b
SantiagoBruno <brunosantiga@gmail.com>
2023-01-25 23:10:27 -0300

    fix: adptRoute return param with error

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -3,16 +3,15 @@ import { Request, Response } from 'express'
 
 export const adptRoute = (controller: Controller) => {
   return (req: Request, res: Response) => {
-    void (async () => {
-      const httpRequest: HttpRequest = {
-        body: req.body
-      }
-      const httpResponse: HttpResponse = await controller.handle(httpRequest)
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    controller.handle(httpRequest).then((httpResponse: HttpResponse) => {
       if (httpResponse.statusCode === 200) {
         res.status(httpResponse.statusCode).json(httpResponse.body)
       } else {
         res.status(httpResponse.statusCode).json(httpResponse.body.message)
       }
-    })
+    }).catch((error) => { throw error })
   }
 }
