07f30265957a3ae8eaf476a721ec38cae4729d7d
SantiagoBruno <brunosantiga@gmail.com>
2023-02-18 17:50:17 -0300

    fix: ensure SignupFactory calls authentication

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,18 +1,28 @@
-import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
+import env from '../../config/env'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
+import { Controller } from '../../../presentation/protocols'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
-import { Controller } from '../../../presentation/protocols'
-import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { makeSignUpValidation } from './signup-validation-factory'
 
 export const makeSignUpController = (): Controller => {
-  const salt = 12
-  const bcryptAdapter: BcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository: AccountMongoRepository = new AccountMongoRepository()
-  const dbAddAccount: DbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const dbAuthentication = new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository
+  )
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), dbAuthentication)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
