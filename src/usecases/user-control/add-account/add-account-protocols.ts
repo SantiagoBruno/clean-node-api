046d43a8514ecea6260698bd9499b2281325669d
SantiagoBruno <brunosantiga@gmail.com>
2023-02-22 11:03:06 -0300

    refactor: move repository usecase protocols to correct folder

--- src/usecases/user-control/add-account/add-account-protocols.ts
+++ src/usecases/user-control/add-account/add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../protocols/criptography/hasher'
-export * from '../../protocols/db/account/add-account-repository'
-export * from '../../protocols/db/account/load-account-by-email-repository'
+export * from '../../protocols/repository/account/add-account-repository'
+export * from '../../protocols/repository/account/load-account-by-email-repository'
