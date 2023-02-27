345e569eaef6dfbda6614f5c4f49a8fd6fe290ba
SantiagoBruno <brunosantiga@gmail.com>
2023-02-26 21:04:17 -0300

    test: ensure addSurvey route returns 204 if access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
+import env from '../config/env'
 import { Collection } from 'mongodb'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
+import { sign } from 'jsonwebtoken'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,8 @@ describe('Login Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST/addsurvey', () => {
@@ -36,6 +41,41 @@ describe('Login Routes', () => {
           ]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      console.log(res)
+      await request(app)
+        .post('/api/addsurvey')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'valid_question',
+          answers: [
+            {
+              answer: 'any_answer_1'
+            },
+            {
+              image: 'any_image_2',
+              answer: 'any_answer_2'
+            }
+          ]
+        })
+        .expect(204)
     })
   })
 })
