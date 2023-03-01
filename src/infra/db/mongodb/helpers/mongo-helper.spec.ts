b87a9266ccd792ca356c439dd15cfa9de898fe98
SantiagoBruno <brunosantiga@gmail.com>
2023-03-01 10:25:06 -0300

    test: ensure mapAccount on MongoHelper returns correct values

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -14,4 +14,19 @@ describe('Mongo Helper', () => {
     accountCollection = await sut.getCollection('accounts')
     expect(accountCollection).toBeTruthy()
   })
+  test('Should convert the account recieved from mongo to the correct format', () => {
+    const account = {
+      _id: 'any_id',
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    }
+    const mappedAccount = sut.mapAccount(account)
+    expect(mappedAccount).toEqual({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+  })
 })
