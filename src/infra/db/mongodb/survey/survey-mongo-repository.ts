7404cc5011bb9c104cf6ad6b4f851be51a891249
SantiagoBruno <brunosantiga@gmail.com>
2023-03-04 16:14:59 -0300

    feat: ensunre SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,21 @@
+import { SurveyModel } from '../../../../domain/models/survey'
 import { AddSurveyRepository } from '../../../../usecases/protocols/repository/survey/add-survey-repository'
+import { LoadSurveysRepository } from '../../../../usecases/protocols/repository/survey/load-surveys-repository'
 import { AddSurveyModel } from '../../../../usecases/survey/add-survey/add-survey-interface'
 import { MongoHelper } from '../helpers/mongo-helpers'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements
+  AddSurveyRepository,
+  LoadSurveysRepository {
   async add (addSurveyModel: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(addSurveyModel)
+  }
+
+  async loadAll (): Promise<SurveyModel[] | []> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const mongoObjSurveys = await surveyCollection.find().toArray()
+    const surveys: SurveyModel[] = MongoHelper.mapMongoDbArrObjects(mongoObjSurveys)
+    return surveys
   }
 }
