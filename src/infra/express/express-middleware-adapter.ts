b93c24981a460688d9a8fc448d18dbccf8a5668b
SantiagoBruno <brunosantiga@gmail.com>
2023-03-05 20:58:00 -0300

    feat: define type of adaptMiddleware return

--- src/infra/express/express-middleware-adapter.ts
+++ src/infra/express/express-middleware-adapter.ts
@@ -1,24 +1,26 @@
 import { HttpRequest, HttpResponse, Middleware } from '../../presentation/protocols'
 import { Request, Response, NextFunction } from 'express'
 
-export const adptMiddleware = (middleware: Middleware) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const httpRequest: HttpRequest = {
-      headers: req.headers
+interface expressMiddlewareFunction {
+  (req: Request, res: Response, next: NextFunction)
+}
+
+export const adptMiddleware = (middleware: Middleware): expressMiddlewareFunction => {
+  return (
+    async (req: Request, res: Response, next: NextFunction) => {
+      const httpRequest: HttpRequest = {
+        headers: req.headers
+      }
+      const httpResponse: HttpResponse = await middleware.handle(httpRequest)
+
+      if (httpResponse.statusCode === 200) {
+        await Object.assign(req, httpResponse.body)
+        next()
+      } else {
+        res.status(httpResponse.statusCode).json({
+          error: httpResponse.body.message
+        })
+      }
     }
-    middleware.handle(httpRequest)
-      .then((httpResponse: HttpResponse) => {
-        if (httpResponse.statusCode === 200) {
-          Object.assign(req, httpResponse.body)
-          next()
-        } else {
-          res.status(httpResponse.statusCode).json({
-            error: httpResponse.body.message
-          })
-        }
-      })
-      .catch((error) => {
-        throw error
-      })
-  }
+  )
 }
