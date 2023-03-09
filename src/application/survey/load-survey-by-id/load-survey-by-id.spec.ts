38cc82e2ae93c3c3dea0d344a0ae1279da830b24
SantiagoBruno <brunosantiga@gmail.com>
2023-03-09 12:45:56 -0300

    test: ensure LoadSurveyById throws if LoadByIdRepository throws

--- src/application/survey/load-survey-by-id/load-survey-by-id.spec.ts
+++ src/application/survey/load-survey-by-id/load-survey-by-id.spec.ts
@@ -59,4 +59,13 @@ describe('LoadSurveys', () => {
     const survey = await sut.loadById('any_id')
     expect(survey).toEqual(makeFakeSurvey())
   })
+
+  test('Should throw if LoadSurveyByIdRepository throws', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById').mockImplementation(async () => {
+      throw new Error()
+    })
+    const promise = sut.loadById('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
