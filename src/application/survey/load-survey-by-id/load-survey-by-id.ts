c9956d70d4c3c94623b084dcc1c46d39fdcc79a0
SantiagoBruno <brunosantiga@gmail.com>
2023-03-09 12:41:24 -0300

    feat: ensure LoadSurveyBiId calls LoadSurveyByIdRepository with correct value

---
+++ src/application/survey/load-survey-by-id/load-survey-by-id.ts
@@ -0,0 +1,14 @@
+import { LoadSurveyByIdRepository } from '@/application/protocols/repository/survey/load-survey-by-id-repository'
+import { LoadSurveyByIdInterface } from './load-survey-by-id-interface'
+import { SurveyModel } from './load-survey-by-id-protocols'
+
+export class LoadSurveyById implements LoadSurveyByIdInterface {
+  constructor (
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {}
+
+  async loadById (id: string): Promise<SurveyModel> {
+    await this.loadSurveyByIdRepository.loadById(id)
+    return null
+  }
+}
